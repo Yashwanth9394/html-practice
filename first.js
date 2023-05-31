dacce8969e59470f744b4192b6e8ce8a32396c3f
Yashwanth9394 <100872831+Yashwanth9394@users.noreply.github.com>
2023-05-31 17:47:40 -0400

    Add files via upload

---
+++ first.js
@@ -0,0 +1,43 @@
+/*var a =15;
+var b =5;
+var c= a+b;
+
+document.write("The value of a is ",a );
+
+document.write("The value of b is ",b );
+
+document.write("The value of c is ",c );
+ {
+    var bike;
+    bike= "Honda";
+    document.write("Bike name is ",bike)
+ }
+ document.write("Bike name is ",bike)*/
+ 
+ /* const x=5,y=7;
+ const z= x+y;
+ document.write(z)
+ x= x+5
+ document.write(x) /*  const won't let you change the 
+ var a = 5;
+ var b = "alpha";
+ var c = true;
+ var d = null;
+ var e; 
+ document.write(typeof(a));
+ document.write(typeof(b));
+ document.write(typeof(c));
+ document.write(typeof(d));
+ document.write(typeof(e));
+ */
+
+var a=9;
+switch(a)
+{
+    case 9:
+        document.write("a is 9");
+        break
+    case 10:
+        document.write("a is 10");
+        break
+}
